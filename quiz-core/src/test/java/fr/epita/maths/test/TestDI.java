a94e630bfd6fe8940b0e89a0c6269e1501da8df2
tbrou <tbrou@tbrou-PC7.natsystem.fr>
2018-09-24 20:35:03 +0200

    * integration with h2 datasource + log4j2 conf

--- quiz-core/src/test/java/fr/epita/maths/test/TestDI.java
+++ quiz-core/src/test/java/fr/epita/maths/test/TestDI.java
@@ -1,7 +1,13 @@
 package fr.epita.maths.test;
 
-import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,15 +17,31 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"/applicationContext.xml"})
 public class TestDI {
+	
+	private static final Logger LOGGER = LogManager.getLogger(TestDI.class);
 
 	@Inject
 	String query;
 	
 	
+	@Inject
+	DataSource ds;
+	
 	@Test
 	public void firstDITest() {
-		System.out.println(query);
+		LOGGER.info(query);
 		Assert.assertNotNull(query);
+	}
+	
+	@Test
+	public void dsInjectionTest() throws SQLException {
+		Connection connection = ds.getConnection();
+		
+		String schema = connection.getSchema();
+		Assert.assertEquals("PUBLIC", schema);
+		
+		LOGGER.info("schema : {}", schema);
+		connection.close();
 	}
 	
 }
