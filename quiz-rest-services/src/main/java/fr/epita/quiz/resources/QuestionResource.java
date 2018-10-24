2433a48b65e384d4e02fa0600faef0cd47c7a092
tbrou <tbrou@tbrou-PC7.natsystem.fr>
2018-10-24 18:49:40 +0200

    * Tx update

--- quiz-rest-services/src/main/java/fr/epita/quiz/resources/QuestionResource.java
+++ quiz-rest-services/src/main/java/fr/epita/quiz/resources/QuestionResource.java
@@ -11,6 +11,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -86,6 +87,21 @@ public class QuestionResource {
 		return Response.ok(message).build();
 	}
 	
+	@PUT
+	@Path("/{id}")
+	@Consumes(value = { MediaType.APPLICATION_JSON })
+	public Response updateOneQuestion(QuestionMessage message) {
+		
+		Question question = qDao.getById(Long.valueOf(message.getId()));
+		if (question == null) {
+			return Response.status(Status.NOT_FOUND).entity("{\"message\" : 'Not found'}").build();
+		}
+		
+		applyToQuestion(message, question);
+		qDao.update(question);
+		return Response.ok(message).build();
+	}
+	
 	private static Question toQuestion(QuestionMessage qm) {
 		Question question = new Question();
 		question.setId(qm.getId());
@@ -97,6 +113,9 @@ public class QuestionResource {
 		questionMessage.setId(question.getId());
 		questionMessage.setQuestionLabel(question.getQuestionLabel());
 		return questionMessage;
+	}
+	private static void applyToQuestion(QuestionMessage qmessage, Question question) {
+		question.setQuestionLabel(qmessage.getQuestionLabel());
 	}
 	
 	//TODO
